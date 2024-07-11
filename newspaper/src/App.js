b1fa5189a3e46e761f69d6d39055584c1bdbd847
seHee <sea11hee@gmail.com>
2024-07-11 14:40:51 +0900

    newsList/ newsItem UI 골격

--- newspaper/src/App.js
+++ newspaper/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
   const [data, setData] = useState(null);
@@ -18,12 +19,7 @@ function App() {
   }
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
+    <NewsList/>
   );
 }
 
